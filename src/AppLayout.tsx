0a0702111709c8f7be6b16ed8c90d1a111604f46
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-07-01 20:02:32 +0200

    feat: add header component and implement little animation

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,16 +1,33 @@
 import styled from "styled-components";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const variants = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
 
 export default function AppLayout() {
   return (
-    <StyledAppLayout>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </StyledAppLayout>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ ease: "easeOut", duration: 3.2 }}
+    >
+      <StyledAppLayout>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </StyledAppLayout>
+    </motion.div>
   );
 }
 
-const StyledAppLayout = styled.div``;
+const StyledAppLayout = styled.div`
+  & > main {
+    background: #11071f;
+  }
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,41 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "/assets/Logo.png";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <img src={logo} alt="logo" />
+      <Navigations>
+        <Link to="/">Home</Link>
+        <Link to="/experience">Projects</Link>
+        <Link to="/skills">Skills</Link>
+      </Navigations>
+    </StyledHeader>
+  );
 }
+
+const StyledHeader = styled.div`
+  width: 100%;
+  background: #1a0b2e;
+  box-shadow: 0px 6px 22px -3px rgba(0, 0, 0, 0.1);
+  padding: 2.7rem 17rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    color: #fff;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: 0.4px;
+    text-decoration: none;
+  }
+`;
+
+const Navigations = styled.div`
+  display: flex;
+  gap: 6rem;
+`;
